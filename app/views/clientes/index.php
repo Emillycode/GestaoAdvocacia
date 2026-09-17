bd18950110dbebd2dc03d6cf49fac0958c0e65ec
Emillycode <lima7emilly12@gmail.com>
2026-09-17 06:59:24 -0300

    ajustando css

--- app/views/clientes/index.php
+++ app/views/clientes/index.php
@@ -32,7 +32,7 @@
                         <td><?= htmlspecialchars($cliente['cpf_cnpj']) ?></td>
                         <td>
                             <div class="d-flex gap-2">
-                                <a href="<?= BASE_URL ?>/clientes/editar/<?= $cliente['id'] ?>" class="btn btn-sm btn-outline-primary" title="Editar"><i class="bi bi-pencil"></i></a>
+                                <a href="<?= BASE_URL ?>/clientes/editar/<?= $cliente['id'] ?>" class="btn btn-sm btn-outline-primary" title="Editar"><i class="bi bi-pencil"></i> Editar</a>
                             <a href="<?= BASE_URL ?>/clientes/excluir/<?= $cliente['id'] ?>" class="btn btn-sm btn-outline-danger" title="Excluir" onclick="return confirm('Tem certeza que deseja excluir este cliente?');"><i class="bi bi-trash"></i></a>
                             </div>
                         </td>
